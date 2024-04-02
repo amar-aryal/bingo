51bc19265cb98c12e725456503b93576332ac5c1
Amar Aryal <amararyal878@gmail.com>
2024-04-02 17:41:26 +0545

    add join room dialog input validation: UI side

--- views/scripts/home_script.js
+++ views/scripts/home_script.js
@@ -1,14 +1,20 @@
 var socket = io();
 
+// for directly creating room
 var createRoom = document.getElementById("create-room");
-var joinRoomDialog = document.getElementById("join-room-dialog");
-var roomInfoDialog = document.getElementById("room-info-dialog");
-var closeDialogBtn = document.getElementById("close-dialog");
 
 createRoom.addEventListener("click", () => {
   socket.emit("JoinRoom", "abcd");
   window.location.replace("bingo.html");
 });
+
+// for joining existing room
+var joinRoomDialog = document.getElementById("join-room-dialog");
+var roomInfoDialog = document.getElementById("room-info-dialog");
+var closeDialogBtn = document.getElementById("close-dialog");
+var joinRoom = document.getElementById("join-room");
+var roomNameInput = document.getElementById("room-name");
+var inputErrorMsg = document.getElementById("input-error-msg");
 
 joinRoomDialog.addEventListener("click", () => {
   roomInfoDialog.showModal();
@@ -16,4 +22,14 @@ joinRoomDialog.addEventListener("click", () => {
 
 closeDialogBtn.addEventListener("click", () => {
   roomInfoDialog.close();
+});
+
+joinRoom.addEventListener("click", () => {
+  if (!roomNameInput.value || roomNameInput.value == "") {
+    inputErrorMsg.innerText = "Room name is empty!";
+    return;
+  } else {
+    inputErrorMsg.innerText = "";
+    var roomName = roomNameInput.value;
+  }
 });
